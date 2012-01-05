39816dcb00279efbb5b1ad391805709e8bf519bc
jeff <leterip@me.com>
2012-01-05 11:27:46 -0500

    some hackin

--- admin.go
+++ admin.go
@@ -2,19 +2,36 @@ package admin
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"reflect"
 )
 
-var collections = make(map[string]reflect.Type)
+type AuthFunc func(*http.Request) bool
+
+//Stores info about a specific collection, like the type of the object it
+//represents and any options used in specifying the type
+type collectionInfo struct {
+	Options *Options
+	Type    reflect.Type
+}
+
+//Options when adding a collection to the admin
+type Options struct {
+	Columns []string
+}
+
+//our main collection mapping variable
+var collections = make(map[string]collectionInfo)
 
 //Registers the type/collection pair in the admin. Panics if two types are mapped
 //to the same collection
-func Register(typ interface{}, collection string) {
+func Register(typ interface{}, collection string, options *Options) {
 	t := reflect.TypeOf(typ)
 	if c, ok := collections[collection]; ok {
 		panic(fmt.Sprintf("collection already registered: %s -> %s", c, t))
 	}
-	collections[collection] = t
+	collections[collection] = collectionInfo{options, t}
 }
 
 //Returns an interface{} that corresponds to a *T where T is the type registered
@@ -25,5 +42,31 @@ func GetType(coll string) interface{} {
 		return nil
 	}
 
-	return reflect.New(t).Interface()
+	return reflect.New(t.Type).Interface()
 }
+
+//Admin is an http.Handler for serving up the admin pages
+type Admin struct {
+	Auth     AuthFunc
+	Database interface{} //the mgo connection
+}
+
+func (a *Admin) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	if a.Auth != nil && !a.Auth(req) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	route := a
+
+	log.Println(req.URL.Path)
+}
+
+type route struct {
+	coll string
+	action 
+}
+
+
+
+func ()
--- admin_test.go
+++ admin_test.go
@@ -5,7 +5,7 @@ import "testing"
 type T struct{}
 
 func TestRegister(t *testing.T) {
-	Register(T{}, "T")
+	Register(T{}, "T", nil)
 
 	ans := GetType("T")
 	if _, ok := ans.(*T); !ok {

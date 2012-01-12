ac4e43afbc255ee2be89db9e692c0e2a5f88dc63
jeff <leterip@me.com>
2012-01-11 19:33:34 -0500

    Moved TemplateContext to render.go

--- form.go
+++ form.go
@@ -70,55 +70,6 @@ type LoadingErrors map[string]error
 //expecting one. For what the keys of the map must be, see LoadingErrors
 type ValidationErrors map[string]error
 
-//TemplateContext is the value passed in as the dot to the template for forms
-//by the default renderer. It has methods for returning the values in the field
-//and any errors in attempting to validate the form. For example if we had the
-//type
-//
-//	type MyForm struct {
-//		X int
-//		Y string
-//	}
-//
-//a simple template that uses the TemplateContext for this struct could look like
-//
-//	func (m *MyForm) GetTemplate() string {
-//		return `<span class="errors">{{.Errors "X"}}</span>
-//			<input type="text" value="{{.Values "X"}}" name="X">
-//			<span class="errors">{{.Errors "Y"}}</span>
-//			<input type="text" value="{{.Values "Y"}}" name="Y">
-//			<input type="submit">`
-//	}
-//
-//The form is rendered through the html/template package and will do necessary
-//escaping as such. It is the renderers responsibility to wrap the fields
-//in the form.
-type TemplateContext struct {
-	Errors map[string]error
-	Values map[string]string
-}
-
-//NewTemplateContext creates a new TemplateContext ready to be used.
-func NewTemplateContext() TemplateContext {
-	return TemplateContext{map[string]error{}, map[string]string{}}
-}
-
-//Error returns any error text from validation for a specific field.
-func (t *TemplateContext) Error(field string) string {
-	if v, ex := t.Errors[field]; ex && v != nil {
-		return v.Error()
-	}
-	return ""
-}
-
-//Value returns the value the user input into the form after validation.
-func (t *TemplateContext) Value(field string) string {
-	if v, ex := t.Values[field]; ex {
-		return v
-	}
-	return ""
-}
-
 //indirect walks up interface/pointer chains until it gets to an actual concrete
 //type. If the pointer is nil, we can't walk up so we get an error.
 func indirect(val reflect.Value) (v reflect.Value, e error) {
--- render.go
+++ render.go
@@ -82,6 +82,55 @@ func (i *IndexContext) Key(db, coll string) string {
 	return fmt.Sprintf("%s.%s", db, coll)
 }
 
+//TemplateContext is the value passed in as the dot to the template for forms
+//by the default renderer. It has methods for returning the values in the field
+//and any errors in attempting to validate the form. For example if we had the
+//type
+//
+//	type MyForm struct {
+//		X int
+//		Y string
+//	}
+//
+//a simple template that uses the TemplateContext for this struct could look like
+//
+//	func (m *MyForm) GetTemplate() string {
+//		return `<span class="errors">{{.Errors "X"}}</span>
+//			<input type="text" value="{{.Values "X"}}" name="X">
+//			<span class="errors">{{.Errors "Y"}}</span>
+//			<input type="text" value="{{.Values "Y"}}" name="Y">
+//			<input type="submit">`
+//	}
+//
+//The form is rendered through the html/template package and will do necessary
+//escaping as such. It is the renderers responsibility to wrap the fields
+//in a form tag.
+type TemplateContext struct {
+	Errors map[string]error
+	Values map[string]string
+}
+
+//NewTemplateContext creates a new TemplateContext ready to be used.
+func NewTemplateContext() TemplateContext {
+	return TemplateContext{map[string]error{}, map[string]string{}}
+}
+
+//Error returns any error text from validation for a specific field.
+func (t *TemplateContext) Error(field string) string {
+	if v, ex := t.Errors[field]; ex && v != nil {
+		return v.Error()
+	}
+	return ""
+}
+
+//Value returns the value the user input into the form after validation.
+func (t *TemplateContext) Value(field string) string {
+	if v, ex := t.Values[field]; ex {
+		return v
+	}
+	return ""
+}
+
 //Form encapsulates a form with a context with the ability to execute and output
 //the correct html.
 type Form struct {
